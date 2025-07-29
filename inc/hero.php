fe0bd650f348dc106e15b44a53ed83f2b11606cb
Yosshiii43 <daisy440440@gmail.com>
2025-07-29 17:49:44 +0900

    update:下層ページheroの外部ファイル化

---
+++ inc/hero.php
@@ -0,0 +1,8 @@
+      <div class="p-hero">
+          <div class="p-hero__bg"></div>
+          <div class="p-hero__body">
+            <div class="p-hero__bodyInner">
+              <p class="p-hero__img"><img src="/img/" alt=""></p>
+            </div>
+          </div>
+      </div> <!-- .p-hero -->
