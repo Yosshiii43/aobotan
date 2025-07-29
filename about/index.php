8421663ae89347af5943273b6298ed4a0430af7d
Yosshiii43 <daisy440440@gmail.com>
2025-07-29 18:58:40 +0900

    add:aboutページ

---
+++ about/index.php
@@ -0,0 +1,20 @@
+<?php
+$pageTitle = '私たちについて | 青牡丹工務店';
+$pageDescription = '大阪府大阪市に拠点をおく青牡丹工務店が、丁寧な家造りで理想を現実にするために行なっていることをご紹介します。';
+$ogImage = '/img/img_ogp.png';
+$ogUrl = 'https://aobotan.yosshiii.site/about/';
+$noIndex = true;
+include $_SERVER['DOCUMENT_ROOT'] . '/inc/head.php';
+?>
+
+<body  class="exp-grid">
+
+  <div class="l-shell">
+    <!-- 全ページ共通のヘッダー -->
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php'; ?>
+
+    <main class="l-main">
+      <!-- コンテンツ -->
+      <p>あばうと</p>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php'; ?>
