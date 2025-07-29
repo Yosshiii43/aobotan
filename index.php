f2f6fa59e34d2035246f78f5acdc4663b0b7b4a3
Yosshiii43 <daisy440440@gmail.com>
2025-07-30 02:50:10 +0900

    update:.c-contactSec スペルミス

--- index.php
+++ index.php
@@ -280,20 +280,20 @@ include $_SERVER['DOCUMENT_ROOT'] . '/inc/head.php';
       <section class="c-contactSec">
         <div class="c-contactSec__inner">
           <div>
-            <h2 class="c-contctSec__title c-topSecTitle c-fadeIn js-fadeIn">
+            <h2 class="c-contactSec__title c-topSecTitle c-fadeIn js-fadeIn">
               <span class="c-topSecTitle__text">お問い合わせ</span>
               <span class="c-topSecTitle__deco">contact</span>
             </h2>
-            <div class="c-contctSec__body">
-              <div class="c-contctSec__tel">
+            <div class="c-contactSec__body">
+              <div class="c-contactSec__tel">
                 <p>Tel</p>
                 <p>お電話</p>
               </div>
-              <div class="c-contctSec__telNumber">
+              <div class="c-contactSec__telNumber">
                 <p>0000-000-0000</p>
                 <p>営業時間10:00-20:00</p>
               </div>
-              <div class="c-contctSec__mail">
+              <div class="c-contactSec__mail">
                 <a  class="c-button--a" href="/contact"><img src="/img/icon_mail01.svg" alt="" width="20" height="20">メールフォームはこちら</a>
               </div>
             </div>
