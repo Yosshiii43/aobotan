032fd17ff2746243e00400f1c2b09ce81bd057f7
Yosshiii43 <daisy440440@gmail.com>
2025-07-30 20:24:33 +0900

    .c-contactSec パーツ化

---
+++ inc/contactSec.php
@@ -0,0 +1,26 @@
+      <section class="c-contactSec">
+        <div class="c-contactSec__inner">
+          <div class="l-inner">
+            <h2 class="c-contactSec__title c-topSecTitle c-fadeIn js-fadeIn">
+              <span class="c-topSecTitle__text">お問い合わせ</span>
+              <span class="c-topSecTitle__deco">contact</span>
+            </h2>
+            <div class="c-contactSec__body">
+              <div class="c-contactSec__tel">
+                <p>Tel</p>
+                <p>お電話</p>
+              </div>
+              <div class="c-contactSec__telNumber">
+                <p>0000-000-0000</p>
+                <p>営業時間10:00-20:00</p>
+              </div>
+              <div class="c-contactSec__mail">
+                <a  class="c-button--a" href="/contact"><img src="/img/icon_mail01.svg" alt="" width="20" height="20">メールフォームはこちら</a>
+              </div>
+            </div>
+            <div class="c-contactSec__catch">
+              <img src="/img/img_contact01.svg" alt="">
+            </div><!-- .p-topService__catch -->
+          </div>
+        </div><!-- .c-contactSec__inner -->
+      </section><!-- .c-contactSec -->
