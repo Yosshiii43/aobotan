f09753ba5f9670c663e0ffc234fc87775fa38ed6
Yosshiii43 <daisy440440@gmail.com>
2025-08-01 18:11:18 +0900

    add:serviceページ

---
+++ service/index.php
@@ -0,0 +1,53 @@
+<?php
+$pageTitle = '事業内容 | 青牡丹工務店';
+$pageDescription = '大阪府大阪市に拠点をおく青牡丹工務店の事業内容についてご説明いたします。';
+$ogImage = '/img/img_ogp.png';
+$ogUrl = 'https://aobotan.yosshiii.site/service/';
+$noIndex = true;
+$heroImage = '/img/img_serviceHero01.jpg';
+$heroImageAlt = '建築模型';
+$heroImageWidth ='2240';
+$heroImageHeight ='1342';
+include $_SERVER['DOCUMENT_ROOT'] . '/inc/head.php';
+?>
+
+<body  class="exp-grid p-service" id="service">
+
+  <div class="l-shell">
+    <!-- 全ページ共通のヘッダー -->
+    <?php
+    $is_top = false;
+    include $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php';
+    ?>
+
+    <main class="l-main">
+      <!-- ヒーロー -->
+      <?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/hero.php'; ?>
+
+      <!-- コンテンツ -->
+      <section class="c-lowerHeaderSection">
+        <div class="l-inner--703">
+          <h1 class="c-lowerHeaderSection__title">事業内容</h1>
+          <div class="c-lowerHeaderSection__text">
+            <p>青牡丹工務店は、「住宅建築・リフォーム」「方針新築・リフォーム」「公共工事」の3つを事業の柱としています。ここではそれぞれご紹介します。</p>
+          </div>
+          <div class="c-lowerHeaderSection__button">
+            <a class="c-button--b" href="#housing">住宅建築・リフォーム</a>
+            <a class="c-button--b" href="#business">法人新築・リフォーム</a>
+            <a class="c-button--b" href="#public">公共工事</a>
+          </div>
+          <div class="c-lowerHeaderSection__catch">
+            <img src="/img/img_service01.svg" alt="">
+          </div>
+        </div>
+      </section>
+
+      <section class="p-aboutPhilosophy" id="philosophy">
+        <div class="l-inner--923">
+          <h2 class="p-aboutphilosophy__title c-aboutH2">経営理念</h2>
+        </div>
+      </section>
+
+
+      <?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/contactSec.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php'; ?>
